6f3e884e8a235983cf174d5a5784b59577c9f4d6
Abdullah Aldzic <abdullah.aldzic@fsk.unsa.ba>
2022-12-05 12:38:03 +0100

    models and relations

---
+++ app/Models/Tag.ts
@@ -0,0 +1,22 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, ManyToMany, manyToMany } from '@ioc:Adonis/Lucid/Orm'
+import BlogPost from './BlogPost'
+
+export default class Tag extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @manyToMany(() => BlogPost, {
+    pivotTable: "blog_posts_tags"
+  })
+  public blogPosts: ManyToMany<typeof BlogPost>
+}
